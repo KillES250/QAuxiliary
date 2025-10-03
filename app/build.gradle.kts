89b89fd70121ee56eab756887cac9e487c4b8282
火因 <3479000843@qq.com>
2025-10-03 16:36:40 +0800

    尝试修复input

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -431,8 +431,7 @@ val synthesizeDistReleaseApksCI by tasks.registering {
     val outputAbiVariants = mapOf(
         "arm32" to arrayOf("armeabi-v7a"),
         "arm64" to arrayOf("arm64-v8a"),
-        "armAll" to arrayOf("armeabi-v7a", "arm64-v8a"),
-        "universal" to arrayOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+        "armAll" to arrayOf("armeabi-v7a", "arm64-v8a")
     )
     val versionName = android.defaultConfig.versionName
     val outputDir = File(project.buildDir, "outputs" + File.separator + "ci")
@@ -447,7 +446,7 @@ val synthesizeDistReleaseApksCI by tasks.registering {
         if (signConfig == null) {
             logger.error("Task :app:synthesizeDistReleaseApksCI: No release signing config found, skip signing")
         }
-        val requiredAbiList = listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+        val requiredAbiList = listOf("armeabi-v7a", "arm64-v8a")  // 移除 x86 和 x86_64
         outputDir.mkdir()
         val options = ZFileOptions().apply {
             alignmentRule = object : AlignmentRule {
